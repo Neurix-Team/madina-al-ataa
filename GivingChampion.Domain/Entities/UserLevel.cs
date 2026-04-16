ee1b0341e82cd544fe5fd0ab1dd8d6bdd2c81c92
HashemKamal <hashem.kamal1992@gmail.com>
2026-04-16 14:47:40 +0200

    Adding Profile Models

---
+++ GivingChampion.Domain/Entities/UserLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GivingChampion.Domain.Entities
+{
+    public class UserLevel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Xp { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Kp { get; set; }
+
+        public Guid LevelId { get; set; }
+        public Level Level { get; set; } 
+    }
+}
