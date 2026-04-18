893cb5174fe263b8f6de002a0d8bf6a6c70502ff
nadarafat2003 <nrafat112003@gmal.com>
2026-04-18 13:48:22 +0200

    Add CRUD  VolunteerOrder

---
+++ GivingChampion.Common/DTO/ServiceRequestDto/CreateServiceRequestDto.cs
@@ -0,0 +1,32 @@
+﻿using GivingChampion.Common.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GivingChampion.Common.DTO.ServiceRequestDto
+{
+    public class CreateServiceRequestDto
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Required skill is required")]
+        [MaxLength(150)]
+        public string RequiredSkill { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Urgency level is required")]
+        public UrgencyLevel UrgencyLevel { get; set; }
+
+        [Required(ErrorMessage = "Schedule date is required")]
+        public DateTime ScheduleDate { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Duration must be between 1 and 1000")]
+        public int Duration { get; set; }
+
+        [MaxLength(1000)]
+        public string BriefDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Partner id is required")]
+        public Guid PartnerId { get; set; }
+    }
+}
---
+++ GivingChampion.Infrastructure/Interfaces/IServiceRequestRepository.cs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GivingChampion.Domain.Entities;
+
+namespace GivingChampion.Persistance.Interfaces
+{
+    public interface IServiceRequestRepository
+    {
+        // Gets all service requests that are not soft deleted
+        Task<List<ServiceRequest>> GetAllAsync();
+
+        // Gets a single service request by its id
+        // Returns null if the request does not exist or is soft deleted
+        Task<ServiceRequest?> GetByIdAsync(Guid id);
+
+        // Gets all service requests with Pending status
+        Task<List<ServiceRequest>> GetPendingAsync();
+
+        // Gets all service requests created by or related to a specific partner
+        Task<List<ServiceRequest>> GetByPartnerIdAsync(Guid partnerId);
+
+        // Adds a new service request to the DbContext
+        // Note: This does not save changes to the database until SaveChangesAsync is called
+        Task AddAsync(ServiceRequest serviceRequest);
+
+        // Marks an existing service request as modified
+        // Note: This does not save changes to the database until SaveChangesAsync is called
+        void Update(ServiceRequest serviceRequest);
+
+        // Soft deletes a service request instead of physically removing it from the database
+        // Usually sets IsDeleted = true and DeletedAt = current date/time
+        void SoftDelete(ServiceRequest serviceRequest);
+
+        // Saves all pending changes to the database
+        Task SaveChangesAsync();
+    }
+}
