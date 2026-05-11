c603a24587d900fdc94f435abbda4e19330516d0
AbdelrahmanHamdyNeurix <abdelrahmanhamdy@neurix.uk>
2026-05-11 15:45:57 +0300

    updated geoquest to not return already started ot completed quests

--- GivingChampion.Infrastructure/Interfaces/IGeoQuestRepository.cs
+++ GivingChampion.Infrastructure/Interfaces/IGeoQuestRepository.cs
@@ -5,7 +5,7 @@ namespace GivingChampion.Persistance.Interfaces
 {
     public interface IGeoQuestRepository
     {
-        Task<PagedList<GeoQuest>> GetAllAsync(PageParameters pageParameters);
+        Task<PagedList<GeoQuest>> GetAllAsync(Guid userId, PageParameters pageParameters);
         Task<GeoQuest?> GetByIdAsync(Guid id);
         Task AddAsync(GeoQuest geoQuest);
         void Update(GeoQuest geoQuest);
